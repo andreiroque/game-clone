a01d1ba5257870e147c2aad5bf5d60ff45233da3
Andrei Roque <roqueandrei0316@gmail.com>
2024-11-20 09:07:38 +0800

    Add 'player' and add a simple player movement

--- python-snake/main.py
+++ python-snake/main.py
@@ -4,6 +4,9 @@ pygame.init()
 screen = pygame.display.set_mode((640, 480))
 clock = pygame.time.Clock()
 running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
 
@@ -12,8 +15,21 @@ while running:
       running = False
 
   screen.fill("black")
+
+  pygame.draw.rect(screen, "white", pygame.Rect(player_pos.x, player_pos.y, 10, 10))
+
+  keys = pygame.key.get_pressed()
+  if keys[pygame.K_w]:
+    player_pos.y -= 300 * dt
+  elif keys[pygame.K_s]:
+    player_pos.y += 300 * dt
+  elif keys[pygame.K_a]:
+    player_pos.x -= 300 * dt
+  elif keys[pygame.K_d]:
+    player_pos.x += 300 * dt
+
   pygame.display.flip()
 
-  clock.tick(60)
+  dt = clock.tick(60) / 1000
 
 pygame.quit()
