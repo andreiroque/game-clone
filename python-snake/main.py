f4b1bb27857010ed0ceb083b10361dc3c1824737
Andrei Roque <roqueandrei0316@gmail.com>
2024-11-20 09:35:31 +0800

    Add and render score counter

--- python-snake/main.py
+++ python-snake/main.py
@@ -2,9 +2,18 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Snake Clone")
 clock = pygame.time.Clock()
 running = True
 dt = 0
+score_counter = 0
+
+font = pygame.font.Font("DejaVuSansMono.ttf", 20)
+text = font.render(f"Score: {score_counter}",  True, (255, 255, 255), (0, 0, 0))
+
+textRect = text.get_rect()
+
+textRect.center = (60, 30)
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
@@ -15,6 +24,8 @@ while running:
       running = False
 
   screen.fill("black")
+
+  screen.blit(text, textRect)
 
   pygame.draw.rect(screen, "white", pygame.Rect(player_pos.x, player_pos.y, 10, 10))
 
