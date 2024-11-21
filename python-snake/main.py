3da93566c95e61f5b4ed24b909299f6a665300e1
Andrei Roque <roqueandrei0316@gmail.com>
2024-11-21 17:33:04 +0800

    Remove unnecessary line

--- python-snake/main.py
+++ python-snake/main.py
@@ -54,7 +54,6 @@ while running:
   pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(food_pos.x, food_pos.y, 10, 10))
 
 
-
   if player_pos.x < 0:
     player_pos.x = screen.get_width()
   elif player_pos.x > screen.get_width():
