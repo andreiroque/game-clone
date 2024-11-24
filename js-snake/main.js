9df21bc4e0ff4d3277acbbb088cebce964477c51
Andrei Roque <roqueandrei0316@gmail.com>
2024-11-24 11:27:44 +0800

    Add keypressed functionality

--- js-snake/main.js
+++ js-snake/main.js
@@ -1,7 +1,8 @@
-let s;
+var s;
+var scl = 20;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
   s = new Snake();
 }
 
@@ -9,4 +10,16 @@ function draw() {
   background(0);
   s.show();
   s.update();
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
+  }
 }
--- js-snake/snake.js
+++ js-snake/snake.js
@@ -5,14 +5,22 @@ class Snake {
     this.xspeed = 1;
     this.yspeed = 0;
 
+    this.dir = function (x, y) {
+      this.xspeed = x;
+      this.yspeed = y;
+    };
+
     this.update = function () {
-      this.x += this.xspeed;
-      this.y += this.yspeed;
+      this.x += this.xspeed * scl;
+      this.y += this.yspeed * scl;
+
+      this.x = constrain(this.x, 0, width - scl);
+      this.y = constrain(this.y, 0, height - scl);
     };
 
     this.show = function () {
       fill(255);
-      rect(this.x, this.y, 10, 10);
+      rect(this.x, this.y, scl, scl);
     };
   }
 }
